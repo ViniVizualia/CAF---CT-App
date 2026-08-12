97e89b24c3f5d84e1f9b4d726c884fb822d8cdc6
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 10:14:16 -0300

    Create page.tsx

---
+++ app/(atleta)/home/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
+
+export default function Page() {
+  return <PlaceholderScreen title="Home do atleta" etapa="Implementado na Etapa 3/5" />;
+}
