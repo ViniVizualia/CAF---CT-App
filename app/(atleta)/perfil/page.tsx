78412422824c535e42e505e4a467f35ec95067ba
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 10:15:31 -0300

    Create page.tsx

---
+++ app/(atleta)/perfil/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
+
+export default function Page() {
+  return <PlaceholderScreen title="Meu perfil" etapa="Implementado na Etapa 3" />;
+}
