f992a2e3433624330946f890a15ffb1ab8d94954
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 10:44:46 -0300

    Delete app/(organizador)/torneios/[tournamentId]/page.tsx

--- app/(organizador)/torneios/[tournamentId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
-
-export default function Page() {
-  return <PlaceholderScreen title="Detalhes do torneio" etapa="Implementado na Etapa 6" />;
-}
