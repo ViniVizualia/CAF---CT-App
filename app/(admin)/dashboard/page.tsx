72321aa4a61283afc9750d57bf4e73a7d3c9d473
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 09:54:40 -0300

    Create page.tsx

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
+
+export default function Page() {
+  return <PlaceholderScreen title="Dashboard Super Admin" etapa="Implementado na Etapa 4" />;
+}
