63cc72a03e15c73a7d3c4facfb37028e58650793
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 09:57:14 -0300

    Create page.tsx

---
+++ app/(admin)/torneios/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
+
+export default function Page() {
+  return <PlaceholderScreen title="Torneios" etapa="Implementado na Etapa 6" />;
+}
