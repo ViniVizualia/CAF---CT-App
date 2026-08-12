8c5e0f88f5aecb3730ae257ff14034d08cd1298d
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 09:56:34 -0300

    Create page.tsx

---
+++ app/(admin)/organizadores/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
+
+export default function Page() {
+  return <PlaceholderScreen title="Organizadores" etapa="Implementado na Etapa 6" />;
+}
