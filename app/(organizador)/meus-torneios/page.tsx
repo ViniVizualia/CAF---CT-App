03a5568e4145b14168f3dc11632c8a1a39805f22
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-08-12 10:41:49 -0300

    Create page.tsx

---
+++ app/(organizador)/meus-torneios/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from "@/components/ui/PlaceholderScreen";
+
+export default function Page() {
+  return <PlaceholderScreen title="Meus torneios" etapa="Implementado na Etapa 6" />;
+}
