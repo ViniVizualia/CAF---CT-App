3ed48ae247f4f44fe3f238a081a129ae707ecf27
ViniVizualia <vinyalbuquerque17@gmail.com>
2026-09-04 20:25:43 -0300

    Create page.tsx

---
+++ app/(atleta)/loja/page.tsx
@@ -0,0 +1,9 @@
+import { UniformPurchase } from '@/components/loja/UniformPurchase'
+
+export default function LojaPage() {
+  return (
+    <main className="min-h-screen px-6 py-8 max-w-md mx-auto">
+      <UniformPurchase />
+    </main>
+  )
+}
